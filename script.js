0a11ef0ed041eca30fb3117d24f7bc02b7316d7b
ramynkmaly90-cyber <ramynkmaly90@gmail.com>
2026-07-30 08:25:34 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const search = document.querySelector("input");
 
+search.addEventListener("keyup", function () {
+    let value = search.value.toLowerCase();
+    let cards = document.querySelectorAll(".song-card");
+
+    cards.forEach(card => {
+        let title = card.querySelector("h2").textContent.toLowerCase();
+
+        if (title.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
