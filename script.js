bbbb3cb4730068e193eec493c3aead1ce8ace221
ramynkmaly90-cyber <ramynkmaly90@gmail.com>
2026-07-31 01:20:20 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const songs = [
 {
 title:"Rahmat Azari - Gorani Hawrami Remix",
-src:"music/Peshraw-Karim-Naska-Galawizh.mp3"
+src:"music/Rahmat Azari Gorani Hawrami Remix.mp3"
 },
 {
 title:"Peshraw Karim - Naska Galawizh",
-src:"music/Peshraw Karim Naska Galawizh.mp3"
+src:"music/Peshraw-Karim-Naska-Galawizh.mp3"
 }
 ];
 
